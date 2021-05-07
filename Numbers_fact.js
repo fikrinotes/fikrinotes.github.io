18276ab0b02b0068f0f57644f4ff696bbc34419e
Fikri Mulyana Setiawan <65882793+fikrimulyanasetiawan@users.noreply.github.com>
2021-05-07 20:57:34 +0700

    Update Numbers_fact.js

--- Numbers_fact.js
+++ Numbers_fact.js
@@ -1,18 +1,25 @@
-//var quote = document.querySelector('.quote');
-//var author = document.querySelector('.author')
-//fetch('https://random-math-quote-api.herokuapp.com')
-//.then(response => response.json())
-//.then(data => {
-//  quote.innerHTML = data['quote'];
-//  author.innerHTML ="- "+ data['author'];
-//})
+var button1 = document.querySelector('.btn-1')
+var button2 = document.querySelector('.btn-2')
 
-
+var quote = document.querySelector('.quote');
+var author = document.querySelector('.author')
 var result1= document.querySelector('.result-1')
 var result2= document.querySelector('.result-2')
+button1.addEventListener('click',random);
+button2.addEventListener('click',custom);
+
+
+
+fetch('https://random-math-quote-api.herokuapp.com')
+.then(response => response.json())
+.then(data => {
+  quote.innerHTML = data['quote'];
+  author.innerHTML ="- "+ data['author'];
+})
+
 
 function random() {
-  //result1.innerHTML = 'Loading';
+  result1.innerHTML = 'Loading...';
   fetch('http://numbersapi.com/random')
   .then((response) =>response.text())
   .then((data)=> {
@@ -21,17 +28,22 @@ function random() {
   });
 }
 function custom() {
-  //result2.innerHTML = 'Loading...';
+  result2.innerHTML = 'Loading...';
   var type = document.querySelector('.form-select').value;
   var input = document.querySelector('.input').value;
   var num =input.toString()
   var url = 'http://numbersapi.com/'+num+'/'+type;
+  if (isNaN(input)) {
+    alert('fill in this form only with numbers');
+  }
+  else {
     fetch(url)
       .then((response) => response.text())
       .then((data) => {
         result2.innerHTML = data;
     
       });
+  }
   
 }
 
