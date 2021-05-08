b13fe2d86d6d3ad204d938913c01a290e5b90aba
Fikri Mulyana Setiawan <65882793+fikrimulyanasetiawan@users.noreply.github.com>
2021-05-08 16:36:03 +0700

    Add files via upload

---
+++ Numbers_fact.js
@@ -0,0 +1,62 @@
+var button1 = document.querySelector('.btn-1')
+var button2 = document.querySelector('.btn-2')
+
+var quote = document.querySelector('.quote');
+var author = document.querySelector('.author')
+var result1= document.querySelector('.result-1')
+var result2= document.querySelector('.result-2')
+button1.addEventListener('click',random);
+button2.addEventListener('click',custom);
+
+
+
+fetch('https://random-math-quote-api.herokuapp.com')
+.then(response => response.json())
+.then(data => {
+  quote.innerHTML = data['quote'];
+  author.innerHTML ="- "+ data['author'];
+})
+
+
+function random() {
+  result1.innerHTML = 'Loading...';
+  fetch('http://numbersapi.com/random/?json')
+  .then(response =>response.json())
+  .then(data=> {
+    result1.innerHTML=data['text'];
+    
+  });
+}
+function custom() {
+  result2.innerHTML = 'Loading...';
+  var type = document.querySelector('.form-select').value;
+  var input = document.querySelector('.input').value;
+  var num =input.toString()
+  var url = 'http://numbersapi.com/'+num+'/'+type+"/?json";
+  if (isNaN(input)) {
+    alert('fill in this form only with numbers');
+  }
+  else {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          result2.innerHTML = data['text'];
+      
+        });
+  }
+  
+}
+
+
+
+
+//button1.addEventListener('click', function(name){
+//fetch('http://numbersapi.com/random')
+//.then(response => response.json())
+//.then((data) => {
+//  alert("hello world");
+
+//})
+
+//.catch(err => alert("error!"))
+//})
