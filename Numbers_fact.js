208f39dfe3b0e106d121473fc53e4adc9188ddfa
Fikri Mulyana Setiawan <65882793+fikrimulyanasetiawan@users.noreply.github.com>
2021-05-08 08:09:46 +0700

    Update Numbers_fact.js

--- Numbers_fact.js
+++ Numbers_fact.js
@@ -1,7 +1,7 @@
 var button1 = document.querySelector('.btn-1')
-var result1= document.querySelector('.result-1')
 button1.addEventListener('click',random);
 function random() {
+  var result1= document.querySelector('.result-1');
   fetch('http://numbersapi.com/random/?json')
   .then(response =>response.json())
   .then(data=> {
