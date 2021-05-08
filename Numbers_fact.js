ab6c545db268213ba57ef88ccd801ed9bdf8469b
Fikri Mulyana Setiawan <65882793+fikrimulyanasetiawan@users.noreply.github.com>
2021-05-08 11:11:13 +0700

    Update Numbers_fact.js

--- Numbers_fact.js
+++ Numbers_fact.js
@@ -3,10 +3,10 @@ button1.addEventListener('click',random);
 function random() {
   var result1= document.querySelector('.result-1');
   result1.innerHTML = "Loading in Process...";
-  fetch('http://numbersapi.com/random/?json')
-  .then(response => response.json())
-  .then(data => {
+  fetch('http://numbersapi.com/random')
+  .then((response) => response.text())
+  .then((data) => {
   alert("hello world") ;
-  result1.innerHTML = data['text'];
+  result1.innerHTML = data;
 })
 }
