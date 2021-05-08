31172e6683824e39dd0f48614cfa2ff148e4770d
Fikri Mulyana Setiawan <65882793+fikrimulyanasetiawan@users.noreply.github.com>
2021-05-08 09:17:16 +0700

    Update Numbers_fact.js

--- Numbers_fact.js
+++ Numbers_fact.js
@@ -2,10 +2,11 @@ var button1 = document.querySelector('.btn-1')
 button1.addEventListener('click',random);
 function random() {
   var result1= document.querySelector('.result-1');
+  result1.innerHTML = "Loading in Process...";
   fetch('https://random-math-quote-api.herokuapp.com')
   .then(response => response.json())
   .then(data => {
-  result1.innerHTML = data['quote'];
-  alert(data['author'];
+  alert("hello world") ;
+  alert(data['author']);
 })
 }
