3522b8d98080578970014ad9092411211b3bf7e8
Fikri Mulyana Setiawan <65882793+fikrinotes@users.noreply.github.com>
2024-04-09 22:27:53 +0700

    Add files via upload

---
+++ Javascriptproject-menu/eid-app/scripts/script.js
@@ -0,0 +1,29 @@
+function push_button() {
+    const loading = document.getElementById('loading');
+    loading.classList.add("fa");
+    loading.classList.add("fa-spinner");
+    loading.classList.add("fa-spin");
+    time = setTimeout(submit_data, 1000);
+}
+
+function submit_data() {
+    var nama = document.getElementById('nama').value;
+    var pesan = document.getElementById('pesan').value;
+    console.log(pesan);
+    // delete input form
+    document.getElementById('nama').value = "";
+    document.getElementById('pesan').value = "";
+
+    // replace all spaces
+    nama = nama.replaceAll(" ", "%20");
+    pesan = pesan.replaceAll(" ", "%20");
+    console.log(nama);
+    console.log(pesan);
+    
+    //encrypt message
+    nama = encodeURIComponent(CryptoJS.AES.encrypt(nama, "rahasia"));
+    pesan = encodeURIComponent(CryptoJS.AES.encrypt(pesan, "rahasia"));
+
+    location.replace("http://127.0.0.1:5500/response.html?nama=" + nama + "&pesan=" + pesan);
+}
+
