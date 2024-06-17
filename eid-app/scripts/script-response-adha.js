7092ebd501636d19eb0dd5115251df1002c416c1
Fikri Mulyana Setiawan <65882793+fikrinotes@users.noreply.github.com>
2024-06-17 09:07:51 +0700

    Create script-response-adha.js

---
+++ eid-app/scripts/script-response-adha.js
@@ -0,0 +1,45 @@
+var heading = document.getElementById("heading");
+var message = document.getElementById("message");
+
+const searchParams = new URLSearchParams(window.location.search);
+var nama = searchParams.get('nama');
+var pesan = searchParams.get('pesan');
+
+// tes
+console.log(nama);
+console.log(pesan);
+// selesai tes
+
+var list_pesan = [
+    "Semoga limpahan rahmat dan berkah Allah SWT senantiasa menyertai kita semua.",
+    "Semoga amal ibadah kita diterima Allah SWT dan kita menjadi pribadi yang lebih baik.",
+    "Semoga kebahagiaan dan kedamaian selalu hadir di hati dan keluarga kita.",
+    "Semoga di hari raya ini, kita dapat menyebarkan cinta dan kasih sayang kepada sesama.",
+    "Taqabbalallahu Minna Wa Minkum, semoga Allah SWT memberikan ampunan atas segala dosa dan kesalahan kita.",
+    "Mari kita saling memaafkan dan mempererat tali silaturahmi." 
+]
+
+var list_ucapan = [
+    "Selamat Hari Raya Idul Adha 1445 H, ",
+    "Selamat Lebaran, "
+];
+
+// decrypt
+var nama_decrypted = CryptoJS.AES.decrypt(nama, "rahasia").toString(CryptoJS.enc.Utf8).replaceAll("%20", " ");
+var pesan_decrypted = CryptoJS.AES.decrypt(pesan, "rahasia").toString(CryptoJS.enc.Utf8).replaceAll("%20", " ");
+if (pesan_decrypted == "") {
+    pesan_decrypted = list_pesan[getRandomInt(7)];
+}
+
+
+var ucapan = list_ucapan[getRandomInt(2)];
+
+console.log(nama_decrypted);
+console.log(pesan_decrypted);
+heading.innerHTML = ucapan + nama_decrypted + "!";
+message.innerHTML = pesan_decrypted;
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
